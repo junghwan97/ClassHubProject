d62e7cf3452efa0b5ac2aa1e515c992d87bcc336
bhsohn <hanmuson1@gmail.com>
2024-05-09 00:16:26 +0900

    Feat:

--- src/main/java/com/example/classhubproject/data/lecture/LectureUploadedRequest.java
+++ src/main/java/com/example/classhubproject/data/lecture/LectureUploadedRequest.java
@@ -3,7 +3,7 @@ package com.example.classhubproject.data.lecture;
 import lombok.Data;
 
 @Data
-public class LectureDto {
+public class LectureUploadedRequest {
     Integer     user_id;
     String      name;
     String      field;
---
+++ src/main/java/com/example/classhubproject/data/lecture/LectureUploadedResponse.java
@@ -0,0 +1,8 @@
+package com.example.classhubproject.data.lecture;
+
+import lombok.Data;
+
+@Data
+public class LectureUploadedResponse {
+    public Boolean uploaded;
+}
