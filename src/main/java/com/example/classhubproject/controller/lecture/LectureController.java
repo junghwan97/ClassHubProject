a8e97887366c889592c8099d3dfae57eaec8b8f4
bhsohn <hanmuson1@gmail.com>
2024-05-09 00:16:26 +0900

    Feat:

--- src/main/java/com/example/classhubproject/controller/lecture/LectureController.java
+++ src/main/java/com/example/classhubproject/controller/lecture/LectureController.java
@@ -1,11 +1,43 @@
 package com.example.classhubproject.controller.lecture;
 
+import com.example.classhubproject.data.favorite.FavoriteDto;
+import com.example.classhubproject.data.lecture.LectureUploadedRequest;
+import com.example.classhubproject.data.lecture.LectureUploadedResponse;
+import com.example.classhubproject.service.lecture.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("lecture")
 public class LectureController {
+    private final LectureService lectureService;
 
+    @Autowired
+    public LectureController(LectureService lectureService) {
+        this.lectureService = lectureService;
+    }
+
+
+    // 강의 업로드
+    @Operation(summary = "강의 업로드", description = "강의를 업로드.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = FavoriteDto.class))),
+                    @ApiResponse(responseCode = "400", description = "실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
+    @PostMapping("upload")
+    public LectureUploadedResponse upload(@RequestBody LectureUploadedRequest request) {
+        return lectureService.upload(request);
+    }
 
 }
--- src/main/java/com/example/classhubproject/service/lecture/LectureService.java
+++ src/main/java/com/example/classhubproject/service/lecture/LectureService.java
@@ -1,9 +1,31 @@
 package com.example.classhubproject.service.lecture;
 
+import com.example.classhubproject.data.lecture.LectureDto;
+import com.example.classhubproject.data.lecture.LectureUploadedRequest;
+import com.example.classhubproject.data.lecture.LectureUploadedResponse;
+import com.example.classhubproject.mapper.lecture.LectureMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class LectureService {
+    private final LectureMapper lectureMapper;
+
+    @Autowired
+    public LectureService(LectureMapper lectureMapper){
+        this.lectureMapper = lectureMapper;
+    }
+
+    public LectureUploadedResponse upload(LectureUploadedRequest request) {
+        var result = new LectureUploadedResponse();
+        result.uploaded = false;
+
+        return result;
+    }
+
 }
