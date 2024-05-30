9197643d0593ffc210a16848a95708737a1c116e
junghwan <jhzzang97@gmail.com>
2024-05-30 22:28:59 +0900

    Fix : 회원 프로필 이미지 수정

--- src/main/java/com/example/classhubproject/controller/user/UserController.java
+++ src/main/java/com/example/classhubproject/controller/user/UserController.java
@@ -98,16 +98,16 @@ public class UserController {
         userService.updateUserInfo(user);
     }
 
-//    @Operation(summary = "회원 이미지 프로플 수정",
-//            description = "회원 이미지를 수정 할 수 있습니다.",
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "회원 이미지 수정 성공", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-//                    @ApiResponse(responseCode = "400", description = "회원 이미지 수정 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-//            }
-//    )
-//    @PostMapping(value = "updateUserImage/{snsId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public void updateUserImage(@PathVariable("snsId") String snsId,
-//                                @RequestPart("multipartFile") MultipartFile file) {
-//        userService.updateUserImage(snsId, file);
-//    }
+    @Operation(summary = "회원 이미지 프로필 수정",
+            description = "회원 이미지를 수정 할 수 있습니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "회원 이미지 수정 성공", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "회원 이미지 수정 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
+    @PostMapping(value = "updateUserImage/{snsId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void updateUserImage(@PathVariable("snsId") String snsId,
+                                @RequestPart("multipartFile") MultipartFile file) {
+        userService.updateUserImage(snsId, file);
+    }
 }
