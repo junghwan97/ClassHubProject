d25c7e23aaa288aa814f87e46792bc937a9f88b3
junghwan <jhzzang97@gmail.com>
2024-05-31 16:29:35 +0900

    Refactor : 커뮤니티 예외처리 수정

--- src/main/java/com/example/classhubproject/exception/ClassHubErrorCode.java
+++ src/main/java/com/example/classhubproject/exception/ClassHubErrorCode.java
@@ -9,8 +9,9 @@ public enum ClassHubErrorCode {
     ALREADY_CART_ERROR("이미 장바구니에 동일한 상품이 있습니다.", HttpStatus.CONFLICT),
     ALREADY_ORDERED_ERROR("이미 보유한 강의가 포함되어 있습니다.", HttpStatus.CONFLICT),
     HAS_PAYMENT_AMOUNT_MISMATCH("결제 금액 비일치", HttpStatus.CONFLICT),
-    NO_DATA_FOUND("조회 데이터 없음", HttpStatus.NOT_FOUND);
-
+    NO_DATA_FOUND("조회 데이터 없음", HttpStatus.NOT_FOUND),
+    POST_COMMUNITY_ERROR("게시글 작성중 문제가 발생했습니다.", HttpStatus.BAD_REQUEST),
+    MODIFY_COMMUNITY_ERROR("게시글 수정중 문제가 발생했습니다.", HttpStatus.BAD_REQUEST);
     private String message;
     private HttpStatus httpStatus;
 
